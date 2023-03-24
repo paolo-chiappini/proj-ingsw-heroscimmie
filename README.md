8ad453acc16d73726d257ab1edca80a19630a058
Paolo Chiappini <paolo.chiappini@mail.polimi.it>
2023-03-24 12:06:55 +0100

    Created module for server

---
+++ README.md
@@ -0,0 +1,6 @@
+# Corso Ingegneria del Software 2023 - Cremona
+
+Se ni’ mondo esistesse un po’ di bene
+e ognun si considerasse suo fratello,
+ci sarebbe meno pensieri e meno pene
+e il mondo ne sarebbe assai più bello.
---
+++ server/src/test/java/it/polimi/ingsw/TestClass.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestClass {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
