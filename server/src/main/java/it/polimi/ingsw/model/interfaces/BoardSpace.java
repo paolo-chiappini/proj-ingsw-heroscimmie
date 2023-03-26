a66121f32387fb00587297671c0261b16d353e34
Paolo Chiappini <paolo.chiappini@mail.polimi.it>
2023-03-26 12:24:01 +0200

    changed canPlaceTile, added javaDoc

--- server/src/main/java/it/polimi/ingsw/model/interfaces/BoardSpace.java
+++ server/src/main/java/it/polimi/ingsw/model/interfaces/BoardSpace.java
@@ -1,23 +1,28 @@
 package it.polimi.ingsw.model.interfaces;
 
 public interface BoardSpace {
-    /** Get the tile that is placed in the space.
+    /**
+     * Get the tile that is placed in the space.
      * @return tile saved in the space.
      */
     GameTile getTile();
 
-    /** Set a single tile to occupy the space.
+    /**
+     * Set a single tile to occupy the space.
+     * The tile is only placed if the space is active and no other tile is
+     * already in place.
      * @param tile tile to be placed inside the space of the board.
      */
     void setTile(GameTile tile);
 
-    /** Check if the current space can be used in the current game.
-     * @return true if the space can be used during the game for placing tiles,
-     *         false otherwise.
+    /**
+     * Check if the current space can be used.
+     * @return true if the space is active and empty (does not contain other tiles).
      */
-    boolean isActive();
+    boolean canPlaceTile();
 
-    /** Remove the tile in the space.
+    /**
+     * Remove the tile in the space.
      * @return the tile contained in the space.
      */
     GameTile removeTile();
