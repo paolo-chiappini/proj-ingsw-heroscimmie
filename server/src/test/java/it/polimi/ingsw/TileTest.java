9c677649176c17226b1e2de4ed5e517fd5f6a26f
Paolo Chiappini <paolo.chiappini@mail.polimi.it>
2023-03-26 14:54:07 +0200

    better tests for Tile

--- server/src/test/java/it/polimi/ingsw/TileTest.java
+++ server/src/test/java/it/polimi/ingsw/TileTest.java
@@ -1,18 +1,21 @@
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.model.interfaces.GameTile;
 import it.polimi.ingsw.model.Tile;
 import it.polimi.ingsw.model.TileType;
-import org.junit.Test;
+
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TileTest {
-    /**
-     * Test getType of Tile
-     */
-    @Test
-    public void testTileCreation() {
-        GameTile tile = new Tile(TileType.CAT);
-        assertEquals(tile.getType(), TileType.CAT);
+    @Nested
+    @DisplayName("When creating tile")
+    class CreationTest {
+        @Test
+        @DisplayName("type should be equal to assigned type")
+        void typeIsEqualToAssigned() {
+            TileType type = TileType.CAT;
+            Tile tile = new Tile(type);
+            assertEquals(tile.getType(), type);
+        }
     }
 }
