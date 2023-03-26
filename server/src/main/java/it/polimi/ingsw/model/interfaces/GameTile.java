abc296236e136021e26445634340ceb3cca6863f
Paolo Chiappini <paolo.chiappini@mail.polimi.it>
2023-03-26 12:28:35 +0200

    added javaDoc to GameTile

--- server/src/main/java/it/polimi/ingsw/model/interfaces/GameTile.java
+++ server/src/main/java/it/polimi/ingsw/model/interfaces/GameTile.java
@@ -2,6 +2,13 @@ package it.polimi.ingsw.model.interfaces;
 
 import it.polimi.ingsw.model.TileType;
 
+/**
+ * Representation of a generic tile that is associated with a type.
+ */
 public interface GameTile {
+    /**
+     * Get the type of tile.
+     * @return the type of tile.
+     */
     TileType getType();
 }
