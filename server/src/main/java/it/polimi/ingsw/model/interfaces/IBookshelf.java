13adbca932a76045e9697c5bb207cfbebcbdada6
Paolo Chiappini <paolo.chiappini@mail.polimi.it>
2023-03-30 16:15:11 +0200

    changed method signature

--- server/src/main/java/it/polimi/ingsw/model/interfaces/IBookshelf.java
+++ server/src/main/java/it/polimi/ingsw/model/interfaces/IBookshelf.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.model.interfaces;
 
 public interface IBookshelf {
-    default GameTile getTile(int row, int column) { return null; }
+    default GameTile getTileAt(int row, int column) { return null; }
     default int getWidth() { return 0; }
     default int getHeight() { return 0; }
     default boolean isFull() { return false; }
