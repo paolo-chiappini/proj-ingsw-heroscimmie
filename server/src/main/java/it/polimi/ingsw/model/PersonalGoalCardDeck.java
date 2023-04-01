ea94bbd068b727c79af50d0a2708a1f5d575bb43
AnnaG58 <102239453+AnnaG58@users.noreply.github.com>
2023-04-01 12:06:43 +0200

    Create PersonalGoalCardDeck.java

---
+++ server/src/main/java/it/polimi/ingsw/model/PersonalGoalCardDeck.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.model;
+
+import java.util.ArrayList;
+
+public class PersonalGoalCardDeck {
+    private ArrayList<PersonalGoalCard> personalGoalCards;
+    public PersonalGoalCardDeck(){
+        personalGoalCards = new ArrayList<PersonalGoalCard>();
+    }
+}
