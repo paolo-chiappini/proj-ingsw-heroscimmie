febca36b980ab2e4bf0bae3edf5e8bf6a7a21609
CamillaVelluti <102239670+CamillaVelluti@users.noreply.github.com>
2023-04-01 10:49:28 +0200

    Added addTiles, exceptions and fixed test

--- server/src/test/java/it/polimi/ingsw/BagTest.java
+++ server/src/test/java/it/polimi/ingsw/BagTest.java
@@ -1,85 +1,100 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.exceptions.IllegalActionException;
 import it.polimi.ingsw.model.Bag;
 import it.polimi.ingsw.model.Tile;
 import it.polimi.ingsw.model.TileType;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import it.polimi.ingsw.model.interfaces.GameTile;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Tests on BagTest")
 class BagTest {
 
     Bag bag;
-    @BeforeEach
-    void setUp() {
-        bag = new Bag();
-    }
 
-    /**
-     * test if the tile is removed from the bag
-     */
-    @Test
-    void drawOneTile() {
-        Tile tile=bag.drawTile();
-        assertEquals(bag.getTilesBag().get(tile.getType()),21);
-    }
+    @Nested
+    @DisplayName("On bag creation")
+    class OnBagCreationTests {
+        @BeforeEach
+        void createBag() {
+            bag = new Bag();
+        }
 
-    /**
-     * test if the bag empties by taking all the tiles
-     */
-    @Test
-    void drawAllTile()
-    {
-        for(int i=0;i<132;i++)
-        {
-            bag.drawTile();
+        @Test
+        @DisplayName("the bag should has 132 tiles inside")
+        void bagSizeAtBeginning() {
+            int countTiles = 0;
+            for (TileType type : bag.getTilesBag().keySet()) {
+                countTiles = countTiles + bag.getTilesBag().get(type);
+            }
+            assertEquals(132,countTiles);
         }
-        assertEquals(bag.getTilesBag().size(),0);
-        assertTrue(bag.getTilesBag().isEmpty());
-    }
+        @Test
+        @DisplayName("the bag should has 6 different types of tiles and each type has 22 tiles")
+        void bagTypeAtBeginning() {
+            assertEquals(6,bag.getTilesBag().size());
+            for (TileType type : bag.getTilesBag().keySet()) {
+                assertEquals(22,bag.getTilesBag().get(type));
+            }
+        }
 
-    /**
-     * test if the bag has 132 tiles
-     */
-    @Test
-    void bagSize() {
-        int countTiles=0;
-        for(TileType type : bag.getTilesBag().keySet())
-        {
-            countTiles=countTiles+bag.getTilesBag().get(type);
+        @Nested
+        @DisplayName("When drawing tiles")
+        class DrawingTests {
+            @Test
+            @DisplayName("one tile should be removed from the bag")
+            void drawOneTile() {
+                GameTile tile = bag.drawTile();
+                assertEquals(21,bag.getTilesBag().get(tile.getType()));
+            }
+            @Test
+            @DisplayName("a random number of tiles should be removed from the bag")
+            void drawNTiles() {
+                int num = (int) (Math.random() * 132);
+                int countTiles = 0;
+                for (int i = 0; i < num; i++) {
+                    bag.drawTile();
+                }
+                for (TileType type : bag.getTilesBag().keySet()) {
+                    countTiles = countTiles + bag.getTilesBag().get(type);
+                }
+                assertEquals(132 - num,countTiles);
+            }
+            @Test
+            @DisplayName("the bag should be empty by taking all the tiles")
+            void drawAllTiles() {
+                for (int i = 0; i < 132; i++) {
+                    bag.drawTile();
+                }
+                assertEquals(0,bag.getTilesBag().size());
+                assertTrue(bag.getTilesBag().isEmpty());
+            }
+            @Test
+            @DisplayName("exception should be raised when bag is empty")
+            void bagFull() {
+                drawAllTiles();
+                assertThrows(IllegalActionException.class,
+                        () -> bag.drawTile());
+            }
         }
-        assertEquals(countTiles,132);
-    }
-
-    /**
-     * test if the bag has 6 different types of tiles and each type has 22 tiles
-     */
-    @Test
-    void bagType() {
-        assertEquals(bag.getTilesBag().size(),6);
-        for(TileType type : bag.getTilesBag().keySet())
-        {
-            assertEquals(bag.getTilesBag().get(type),22);
+        @Nested
+        @DisplayName("When adding tiles")
+        class AddingTests {
+            @Test
+            @DisplayName("one tile should be adding to the bag")
+            void addOneTile() {
+                GameTile tile = bag.drawTile();
+                bag.addTile(new Tile(tile.getType()));
+                assertEquals(22,bag.getTilesBag().get(tile.getType()));
+            }
+            @Test
+            @DisplayName("exception should be raised when bag is full")
+            void bagFull() {
+                assertThrows(IllegalActionException.class,
+                        () -> bag.addTile(new Tile(TileType.BOOK)));
+            }
         }
-    }
-
-    /**
-     * test the extraction of a random number of tiles
-     */
-    @Test
-    void drawNTiles()
-    {
-        int num = (int) (Math.random()*132);
-        int countTiles=0;
-        for(int i=0;i<num;i++)
-        {
-            bag.drawTile();
-        }
-        for(TileType type : bag.getTilesBag().keySet())
-        {
-            countTiles=countTiles+bag.getTilesBag().get(type);
-        }
-        assertEquals(countTiles,132-num);
     }
 }
