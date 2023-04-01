9a1301f793ec01f5fd17d17565bde4eced6f3944
AnnaG58 <102239453+AnnaG58@users.noreply.github.com>
2023-04-01 15:50:53 +0200

    Update PersonalGoalCardDeck.java

--- server/src/main/java/it/polimi/ingsw/model/PersonalGoalCardDeck.java
+++ server/src/main/java/it/polimi/ingsw/model/PersonalGoalCardDeck.java
@@ -4,7 +4,21 @@ import java.util.ArrayList;
 
 public class PersonalGoalCardDeck {
     private ArrayList<PersonalGoalCard> personalGoalCards;
+    public static final int NUMBER_OF_PERSONAL_GOAL_CARDS = 12;
+
     public PersonalGoalCardDeck(){
         personalGoalCards = new ArrayList<PersonalGoalCard>();
+        for(int i = 0; i< NUMBER_OF_PERSONAL_GOAL_CARDS; i++)
+            personalGoalCards.add(new PersonalGoalCard(i+1));
+    }
+
+    /**
+     * Draw a random card from the personal goal cards deck
+     * @return cardDrawn
+     */
+    public PersonalGoalCard drawCard(){
+        int randomCardDrawnId = (int)(Math.random()*personalGoalCards.size());
+        PersonalGoalCard cardDrawn = new PersonalGoalCard(randomCardDrawnId);
+        return  cardDrawn;
     }
 }
