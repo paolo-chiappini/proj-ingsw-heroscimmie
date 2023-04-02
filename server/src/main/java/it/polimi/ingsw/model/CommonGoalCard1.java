b1fd8c4d35a04954617d6e8e998f1372f0f5c944
CamillaVelluti <102239670+CamillaVelluti@users.noreply.github.com>
2023-04-02 17:44:53 +0200

    Added CommonGoalCard1 class and test

---
+++ server/src/main/java/it/polimi/ingsw/model/CommonGoalCard1.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.interfaces.IBookshelf;
+import it.polimi.ingsw.util.BookshelfFloodFill;
+
+import java.util.List;
+
+public class CommonGoalCard1 extends CommonGoalCard{
+
+    public CommonGoalCard1(int numPlayer) {
+        super(1,numPlayer);
+    }
+
+    /**
+     * @param bookshelf is the bookshelf on which the achievements of a goal are checked
+     * Pattern to achieve: six groups each containing at least 2 tiles of the same type (not necessarily
+     * in the depicted shape). The tiles of one group can be different from those of another group.
+     * @return true if the goal is achieved
+     */
+    @Override
+    public boolean canObtainPoints(IBookshelf bookshelf) {
+        List<Integer> groupsAdjacency;
+        groupsAdjacency= BookshelfFloodFill.getTileGroupsSizes(bookshelf);
+        return groupsAdjacency.stream().anyMatch(sizeGroups->sizeGroups>=2);
+    }
+}
---
+++ server/src/test/java/it/polimi/ingsw/CommonGoalCard1Test.java
@@ -0,0 +1,111 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.mock.DynamicTestBookshelf;
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.model.interfaces.IBookshelf;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DisplayName("Tests on CommonGoalCard1")
+public class CommonGoalCard1Test {
+
+    CommonGoalCard1 card1;
+
+    @Nested
+    @DisplayName("On card creation")
+    class CardCreationTests {
+        @BeforeEach
+        void createNewCard() {
+            card1 = new CommonGoalCard1(2);
+        }
+
+        @Test
+        @DisplayName("the card id should be 1")
+        void cardId() {
+            assertEquals(1, card1.getId());
+        }
+
+        @Nested
+        @DisplayName("When checking if the card's goal is achieved")
+        class CanObtainPointsTests {
+
+            @Test
+            @DisplayName("should be true with 6 groups each containing at least 2 tiles of the same type")
+            void canObtainPoints6Groups2Tiles() {
+                int[][] template = new int[][] {
+                        { 5,  3, -1, -1,  1},
+                        { 3,  0,  0,  1,  4},
+                        { 5,  1,  5,  3,  0},
+                        { 4,  1,  5,  5,  4},
+                        { 5,  3,  3,  2,  5},
+                        { 0,  0,  2,  2,  1}
+                };
+                IBookshelf bookshelf= new DynamicTestBookshelf(template);
+                assertTrue(card1.canObtainPoints(bookshelf));
+            }
+
+            @Test
+            @DisplayName("should be true with 6 groups each containing more than 2 tiles of the same type")
+            void canObtainPoints6GroupsMoreTiles()  {
+                int[][] template = new int[][] {
+                        { 5,  0, -1, -1,  1},
+                        { 3,  0,  0,  1,  1},
+                        { 1,  1,  5,  3,  0},
+                        { 4,  1,  5,  5,  4},
+                        { 5,  0,  5,  2,  5},
+                        { 0,  0,  2,  2,  1}
+                };
+                IBookshelf bookshelf= new DynamicTestBookshelf(template);
+                assertTrue(card1.canObtainPoints(bookshelf));
+            }
+
+            @Test
+            @DisplayName("should be true with more than 6 groups containing at least 2 tiles of the same type")
+            void canObtainPointsMoreGroups()  {
+                int[][] template = new int[][] {
+                        { 5,  5,  2,  2,  1},
+                        { 3,  0,  0,  1,  4},
+                        { 5,  1,  5,  3,  0},
+                        { 4,  1,  5,  5,  4},
+                        { 5,  3,  3,  2,  5},
+                        { 0,  0,  2,  2,  1}
+                };
+                IBookshelf bookshelf= new DynamicTestBookshelf(template);
+                assertTrue(card1.canObtainPoints(bookshelf));
+            }
+
+            @Test
+            @DisplayName("should be false with less than 6 groups containing at least 2 tiles of the same type")
+            void canNotObtainPointsLessGroups() {
+                int[][] template = new int[][] {
+                        { 5,  3, -1, -1,  1},
+                        { 3,  0,  0,  1,  4},
+                        { 5,  1,  5,  3,  0},
+                        { 4,  1,  5,  5,  4},
+                        { 5,  2,  3,  2,  5},
+                        { 0,  1,  2,  2,  1}
+                };
+                IBookshelf bookshelf= new DynamicTestBookshelf(template);
+                assertTrue(card1.canObtainPoints(bookshelf));
+            }
+
+            @Test
+            @DisplayName("should be false with no groups containing at least 2 tiles of the same type")
+            void canNotObtainPointsNoGroups() {
+                int[][] template = new int[][] {
+                        { 5,  3, -1, -1,  1},
+                        { 3,  5,  0,  1,  4},
+                        { 5,  1,  5,  3,  0},
+                        { 4,  0,  2,  5,  4},
+                        { 5,  3,  4,  2,  5},
+                        { 0,  1,  2,  0,  1}
+                };
+                IBookshelf bookshelf= new DynamicTestBookshelf(template);
+                assertFalse(card1.canObtainPoints(bookshelf));
+            }
+        }
+    }
+}
