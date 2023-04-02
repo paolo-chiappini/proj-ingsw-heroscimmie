9e0cb24a3b8e1137af19c3fff50bfc7563352288
CamillaVelluti <102239670+CamillaVelluti@users.noreply.github.com>
2023-04-02 17:49:46 +0200

    Added CommonGoalCard5 class and test

---
+++ server/src/main/java/it/polimi/ingsw/model/CommonGoalCard5.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.interfaces.IBookshelf;
+
+public class CommonGoalCard5 extends CommonGoalCard {
+    public CommonGoalCard5(int numPlayer) {
+        super(5, numPlayer);
+    }
+
+    /**
+     * @param bookShelf is the bookshelf on which the achievements of a goal are checked
+     * Pattern to achieve: Four tiles of the same type in the four corners of the bookshelf.
+     * @return true if the goal is achieved
+     */
+    @Override
+    public boolean canObtainPoints(IBookshelf bookShelf) {
+        return ((bookShelf.compareTiles(0,0,0, bookShelf.getWidth()-1))
+                &&(bookShelf.compareTiles(0,bookShelf.getWidth()-1,bookShelf.getHeight()-1,bookShelf.getWidth()-1))
+                &&(bookShelf.compareTiles(bookShelf.getHeight()-1,bookShelf.getWidth()-1,bookShelf.getHeight()-1,0))
+                && bookShelf.hasTile(bookShelf.getHeight()-1,0));
+    }
+}
