16ca4546d6da3ba317686453dec5153b98a6b241
AnnaG58 <102239453+AnnaG58@users.noreply.github.com>
2023-04-02 18:54:59 +0200

    Update BoardTest.java

--- server/src/test/java/it/polimi/ingsw/BoardTest.java
+++ server/src/test/java/it/polimi/ingsw/BoardTest.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw;
+import it.polimi.ingsw.model.Bag;
 import it.polimi.ingsw.model.Board;
 import it.polimi.ingsw.model.Bookshelf;
 import it.polimi.ingsw.model.TileSpace;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
@@ -18,20 +20,33 @@ public class BoardTest {
      * Test that board needs to be refilled because it's empty
      */
     @Test
+    @DisplayName("Board is empty")
     void isBoardEmpty(){
         assertTrue(myBoard.needsRefill());
+    }
+
+    /**
+     * Test that board doesn't need to be refilled because it's full
+     */
+    @Test
+    @DisplayName("Board is full, doesn't need to be refill")
+    void isBoardFull(){
+        myBoard.refill(new Bag());
+        assertFalse(myBoard.needsRefill());
     }
 
     /**
      * Test if player can pickUp tiles from tileSpace
      */
     @Test
-    void tilesCanBePickedUp(){          //!!!!!!
+    @DisplayName("")
+    void tilesCanBePickedUp(){
+        myBoard.refill(new Bag());
         int row1 = (int)(Math.random()*6);
         int row2 = (int)(Math.random()*6);
         int col1 = (int)(Math.random()*5);
         int col2 = (int)(Math.random()*5);
-        assertFalse(myBoard.canPickUpTiles(row1, col1, row2, col2));
+        assertTrue(myBoard.canPickUpTiles(row1, col1, row2, col2));
     }
 
     /**
@@ -39,6 +54,7 @@ public class BoardTest {
      */
     @Test
     void pickUp2DifferentTiles(){
+        myBoard.refill(new Bag());
         int row1 = (int)(Math.random()* 6);
         int row2 = (int)(Math.random()*6);
 
