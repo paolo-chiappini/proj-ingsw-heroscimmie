863a1485427da0643b7fa6d93502a63732eb7150
AnnaG58 <102239453+AnnaG58@users.noreply.github.com>
2023-04-04 15:50:13 +0200

    Update Board.java

--- server/src/main/java/it/polimi/ingsw/model/Board.java
+++ server/src/main/java/it/polimi/ingsw/model/Board.java
@@ -7,7 +7,7 @@ public class Board {
     private TileSpace[][] spaces;
 
     public Board(){
-
+        spaces = new TileSpace[BOARD_DIM][BOARD_DIM];
 
     }
 
@@ -16,33 +16,40 @@ public class Board {
      * @return true if board is empty or there's no adjacency among the cards left on the board
      */
     public boolean needsRefill(){
-        for(int i = 1; i < spaces.length -1; i++){
-            for(int j = 1; j < spaces[0].length -1; j++)
-                if(spaces[i-1][j] == null && spaces[i][j-1] == null && spaces[i+1][j] == null && spaces[i][j+1] == null)
-                    return true;
+        for(int i = 0; i < spaces.length; i++) {
+            for(int j = 0; j < spaces[0].length; j++) {
+                if((i==0) || (j==0)) {
+                    if(spaces[i+1][j]!=null || spaces[i][j+1]!=null) return false;
+                }
+                else if((i==8) || (j==8)) {
+                    if(spaces[i-1][j]!=null || spaces[i][j-1]!=null) return false;
+                }
+                else {
+                    if(spaces[i+1][j]!=null || spaces[i-1][j]!=null || spaces[i][j+1]!=null || spaces[i][j-1]!=null) return false;
+                }
+            }
         }
-        return false;
+        return true;
     }
 
     /**
      *
-     * @param bag
      * Tiles are drawn from the bag and put on the board
+     * @param bag it is the bag from which the tiles are drawn
      */
     public void refill(Bag bag){
-        for(int i = 0; i < spaces.length; i++)
-            for(int j = 0; j < spaces[0].length; j++)
-                if(spaces[i][j].canPlaceTile())
-                    bag.drawTile();
+        for (TileSpace[] space : spaces)
+            for (int j = 0; j < spaces[0].length; j++)
+                if (space[j].canPlaceTile())
+                    space[j].setTile(bag.drawTile());
     }
 
     /**
-     *
-     * @param row1
-     * @param col1
-     * @param row2
-     * @param col2
-     * @return tiles picked up from the board. They can be one, two or three tiles
+     * @param row1 is the row index of the first tile to pick up
+     * @param col1 is the column index of the first tile to pick up
+     * @param row2 is the row index of the last tile to pick up
+     * @param col2 is the column index of the last tile to pick up
+     * @return the list of tiles picked up from the board
      */
     public List<TileSpace> pickUpTiles(int row1, int col1, int row2, int col2){
         List<TileSpace> myList = new LinkedList<>();
@@ -53,7 +60,7 @@ public class Board {
             spaces[row1][col1].removeTile();
         }
 
-        else if(row1 == row2 && col1 != col2){  //player wants to pick two or three tiles
+        else if(row1 == row2){  //player wants to pick two or three tiles
             if(col2 > col1){
                 numOfTilesPicked = col2 - col1 + 1;
                 if(numOfTilesPicked == 2 || numOfTilesPicked == 3)
@@ -62,7 +69,7 @@ public class Board {
                         spaces[row1][col1 + i].removeTile();
                     }
             }
-            else if (col1 > col2) {
+            else {
                 numOfTilesPicked = col1 - col2 + 1;
                 if(numOfTilesPicked == 2 || numOfTilesPicked == 3)
                     for (int i = 0; i < numOfTilesPicked; i++) {
@@ -72,7 +79,7 @@ public class Board {
             }
 
         }
-        else if(row1 != row2 && col1 == col2){
+        else if(col1 == col2){
             if(row2 > row1){
                 numOfTilesPicked = row2 - row1 + 1;
                 if(numOfTilesPicked == 2 || numOfTilesPicked == 3)
@@ -81,7 +88,7 @@ public class Board {
                         spaces[row1 + i][col1].removeTile();
                     }
             }
-            else if (row1 > row2) {
+            else{
                 numOfTilesPicked = row1 - row2 + 1;
                 if(numOfTilesPicked == 2 || numOfTilesPicked == 3)
                     for (int i = 0; i < numOfTilesPicked; i++) {
@@ -91,20 +98,46 @@ public class Board {
             }
         }
         return myList;
+
     }
 
     /**
-     *
-     * @param row1
-     * @param col1
-     * @param row2
-     * @param col2
-     * @return true if player can pick up a tile; false if board in position [row1][col1]
-     *          or [row2][col2] is empty
+     * @param row1 is the row index of the first tile to pick up
+     * @param col1 is the column index of the first tile to pick up
+     * @param row2 is the row index of the last tile to pick up
+     * @param col2 is the column index of the last tile to pick up
+     * @return true if player can pick up a tile;
      */
-    public boolean canPickUpTiles(int row1, int col1, int row2, int col2){
-        if(spaces[row1][col1] !=null && spaces[row2][col2] != null)
-            return true;
+    public boolean canPickUpTiles(int row1, int col1, int row2, int col2) {
+
+        if (row1 == row2) {
+            while (col1 <= col2) {
+                if (spaces[row1][col1] == null || hasFreeSide(row1, col1)) return false;
+                col1++;
+
+            }
+        }
+        if (col1 == col2) {
+            while (row1 <= row2) {
+                if (spaces[row1][col1] == null || hasFreeSide(row1, col1)) return false;
+                row1++;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @param row1 is the row index of the tile to pick up
+     * @param col1 is the column index of the tile to pick up
+     * @return true if the tile has at least one side free ;
+     */
+    public boolean hasFreeSide(int row1, int col1)
+    {
+        if(row1>0 && row1<8 && col1>0 && col1<8)
+        {
+            return spaces[row1 + 1][col1] != null || spaces[row1 - 1][col1] != null
+                    || spaces[row1][col1 + 1] != null || spaces[row1][col1 - 1] != null;
+        }
         return false;
     }
 }
