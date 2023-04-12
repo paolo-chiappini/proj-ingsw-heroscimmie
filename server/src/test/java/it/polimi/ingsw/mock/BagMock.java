152cdb3bd8608c54d6bfe25866cc8c9a0690a720
pciapein <paolo.chiappini02@gmail.com>
2023-04-12 21:44:27 +0200

    added new mock classes

---
+++ server/src/test/java/it/polimi/ingsw/mock/BagMock.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.mock;
+
+import it.polimi.ingsw.model.TileType;
+import it.polimi.ingsw.model.interfaces.GameTile;
+import it.polimi.ingsw.model.interfaces.IBag;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BagMock implements IBag {
+    HashMap<TileType, Integer> tiles;
+
+    public BagMock(Map<TileType, Integer> tiles) {
+        this.tiles = new HashMap<>(tiles);
+    }
+
+    @Override
+    public HashMap<TileType, Integer> getTilesBag() {
+        return new HashMap<>(tiles);
+    }
+
+    @Override
+    public GameTile drawTile() {
+        return null;
+    }
+
+    @Override
+    public void addTile(GameTile tile) {}
+
+    @Override
+    public GameTile getTileByType(TileType type) {
+        return null;
+    }
+}
---
+++ server/src/test/java/it/polimi/ingsw/mock/BoardMock.java
@@ -0,0 +1,56 @@
+package it.polimi.ingsw.mock;
+
+import it.polimi.ingsw.model.Tile;
+import it.polimi.ingsw.model.TileSpace;
+import it.polimi.ingsw.model.TileType;
+import it.polimi.ingsw.model.interfaces.GameTile;
+import it.polimi.ingsw.model.interfaces.IBag;
+import it.polimi.ingsw.model.interfaces.IBoard;
+
+import java.util.List;
+
+public class BoardMock implements IBoard {
+    int size;
+    TileSpace[][] spaces;
+
+    public BoardMock(int size, int[][] template) {
+        this.size = size;
+        spaces = new TileSpace[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                spaces[i][j] = new TileSpace(0, 0);
+                spaces[i][j].setTile(new Tile(TileType.values()[template[i][j]]));
+            }
+        }
+    }
+
+    @Override
+    public boolean needsRefill() {
+        return false;
+    }
+
+    @Override
+    public void refill(IBag bag) {
+
+    }
+
+    @Override
+    public List<TileSpace> pickUpTiles(int row1, int col1, int row2, int col2) {
+        return null;
+    }
+
+    @Override
+    public boolean canPickUpTiles(int row1, int col1, int row2, int col2) {
+        return false;
+    }
+
+    @Override
+    public GameTile getTileAt(int row, int col) {
+        return spaces[row][col].getTile();
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+}
