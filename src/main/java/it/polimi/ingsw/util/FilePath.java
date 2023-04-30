d97b9b760b1f6a0f521b046a36b169738cf5ef93
Paolo Chiappini <paolo.chiappini@mail.polimi.it>
2023-04-30 22:17:02 +0200

    tests for file manager

--- src/main/java/it/polimi/ingsw/util/FilePath.java
+++ src/main/java/it/polimi/ingsw/util/FilePath.java
@@ -4,9 +4,10 @@ package it.polimi.ingsw.util;
  * Predefined paths for managing local resources.
  */
 public enum FilePath {
-    DEFAULT("./server/resources/data"),
+    DEFAULT("./resources/data"),
     TEMPLATES(DEFAULT.getPath() + "/templates"),
-    SAVED(DEFAULT.getPath() + "/saved");
+    SAVED(DEFAULT.getPath() + "/saved"),
+    TEST("./src/test/test_files");
 
     private final String path;
     FilePath(String path) {
