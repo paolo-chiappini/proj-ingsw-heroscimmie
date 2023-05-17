8b0d04ccfac048aae8428f40d773a9c1fbbcbe22
CamillaVelluti <102239670+CamillaVelluti@users.noreply.github.com>
2023-05-17 23:43:38 +0200

    Updated Client

--- src/main/java/it/polimi/ingsw/client/Client.java
+++ src/main/java/it/polimi/ingsw/client/Client.java
@@ -1,57 +1,49 @@
 package it.polimi.ingsw.client;
 
 import it.polimi.ingsw.server.messages.Message;
+import it.polimi.ingsw.server.messages.MessageProvider;
+import it.polimi.ingsw.server.messages.MessageType;
 import it.polimi.ingsw.util.observer.ClientObservable;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.InetSocketAddress;
+import java.io.*;
 import java.net.Socket;
+import java.util.function.Consumer;
 
 public class Client extends ClientObservable {
 
     private final Socket socket;
-    private ObjectOutputStream toServer;
-    private ObjectInputStream fromServer;
-    private String username;
+    private final BufferedReader fromServer;
 
     protected Message message;
+    private final MessageProvider messageProvider = new MessageProvider(MessageType.JSON);
+    private Consumer<Message> callback;
+    private static final int SERVER_PORT=49152;
 
-    public Client(String address, int port){
-        this.socket = new Socket();
+    public Client(String address){
         try {
-            this.socket.connect(new InetSocketAddress(address,port));
-            this.toServer = new ObjectOutputStream(socket.getOutputStream());
-            this.fromServer = new ObjectInputStream(socket.getInputStream());
+            this.socket = new Socket(address, SERVER_PORT);
+            fromServer = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+        // default callback
+        callback = (msg) -> {};
     }
 
-    public void readMessageFromServer() {
-        new Thread(() -> {
-            while(socket.isConnected())
-            {
-                try{
-                    Message message = (Message) fromServer.readObject();
-                    notifyObservers(obs->obs.update(message));
-                }catch(Exception e){
-                    endConnection();
-                }
-            }
-
-        }).start();
+    public void sendRequest(String method, String body) {
+        Message request = messageProvider.getEmptyInstance(socket);
+        request.setMethod(method);
+        request.setBody(body);
+        request.send();
     }
 
-    public void sendMessageToServer(Message message) {
+    public Message readFromServer() {
         try {
-            toServer.writeObject(message);
-            toServer.flush();
+            return messageProvider.getInstanceForIncomingRequest(this.socket, fromServer.readLine());
         } catch (IOException e) {
-            e.printStackTrace();
             endConnection();
         }
+        return null;
     }
 
     public void endConnection() {
@@ -64,16 +56,18 @@ public class Client extends ClientObservable {
         }
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    private void run() {
+        while (true) {
+            Message serverMessage = readFromServer();
+            callback.accept(serverMessage);
+        }
     }
 
-    public String getUsername() {
-        return username;
+    public void setOnMessageReceivedCallback(Consumer<Message> callback) {
+        this.callback = callback;
     }
 
-    public Socket getSocket() {
-        return socket;
+    public void start() {
+        new Thread(this::run).start();
     }
 }
-
