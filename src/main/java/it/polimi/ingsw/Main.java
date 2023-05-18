b682b0e470f93620d3ed1005faa883557c98675d
Paolo Chiappini <paolo.chiappini@mail.polimi.it>
2023-05-18 20:00:37 +0200

    fixed drop tiles bug

--- src/main/java/it/polimi/ingsw/Main.java
+++ src/main/java/it/polimi/ingsw/Main.java
@@ -1,43 +1,8 @@
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.server.Server;
-import it.polimi.ingsw.server.ServerHandlers;
-import it.polimi.ingsw.server.messages.MessageType;
-
 public class Main {
 
     public static void main(String[] args) {
-        Server server = new Server(49152, MessageType.JSON);
-
-        server.setCallback("JOIN", ServerHandlers::handleJoinGame);
-        server.setCallback("NEW",  ServerHandlers::handleNewGame);
-        server.setCallback("LOAD", ServerHandlers::handleLoadGame);
-        server.setCallback("SAVE", ServerHandlers::handleSaveGame);
-        server.setCallback("LIST", ServerHandlers::handleShowSavedGames);
-        server.setCallback("QUIT", ServerHandlers::handlePlayerLeaving);
-        server.setCallback("PICK", ServerHandlers::handleBoardTilePickUp);
-        server.setCallback("DROP", ServerHandlers::handleDropTiles);
-        server.setCallback("NEXT", ServerHandlers::handleEndTurn);
-        server.setCallback("CHAT", (request, response) -> {
-            response = request;
-            response.sendToAll();
-        });
-
-
-        server.onConnectionLost(ServerHandlers::handleDisconnection);
-
-        server.setMiddleware("JOIN", (req, res, callback) -> {
-            callback.call(req, res);
-            ServerHandlers.handleGameStart(req, res);
-        });
-
-        server.setGlobalMiddleware((req, res, next) -> {
-            if (ServerHandlers.validateRequestBody(req, res) && ServerHandlers.validateSocketUsername(req, res)) {
-                next.call(req, res);
-            }
-        });
-
-        server.start();
-
+        // Startup logic with command line arguments
     }
 }
