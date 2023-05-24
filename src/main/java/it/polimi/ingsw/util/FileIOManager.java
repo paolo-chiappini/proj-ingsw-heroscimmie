4ff6a9dfb49428ec49e7ef9ecc894501f44672df
Paolo Chiappini <paolo.chiappini@mail.polimi.it>
2023-05-24 22:34:51 +0200

    new javadoc removed tests

--- src/main/java/it/polimi/ingsw/util/FileIOManager.java
+++ src/main/java/it/polimi/ingsw/util/FileIOManager.java
@@ -1,16 +1,21 @@
 package it.polimi.ingsw.util;
 
 import java.io.*;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Library class used for simple file IO operations in the resources' folder.
  */
 public abstract class FileIOManager {
+    /**
+     * Writes data to a file at the specified path.
+     * @param filePath path to file (excluding filename).
+     * @param filename name of the file.
+     * @param data data to write.
+     * @throws IOException when unable to read from file.
+     */
     public static void writeDataToFile(String filePath, String filename, String data) throws IOException {
         File directory = new File(filePath);
         if (!directory.exists()) {
@@ -22,16 +27,32 @@ public abstract class FileIOManager {
         bufferedWriter.close();
     }
 
+    /**
+     * Reads data from a resource file. File must be a resource to be read.
+     * @param resourcePath path to resource (including filename).
+     * @return the data read from the file.
+     */
     public static String readDataFromResource(String resourcePath) {
         InputStream inputStream = FileIOManager.class.getResourceAsStream(resourcePath);
         return getAllDataFromStream(inputStream);
     }
 
+    /**
+     * Reads data from a file at the specified path.
+     * @param filePath path to file (including filename).
+     * @return the data read from the file.
+     * @throws FileNotFoundException when the file cannot be located.
+     */
     public static String readDataFromFile(String filePath) throws FileNotFoundException {
         InputStream fileInputStream = new FileInputStream(filePath);
         return getAllDataFromStream(fileInputStream);
     }
 
+    /**
+     * Reads all data from the given stream.
+     * @param inputStream stream to read from.
+     * @return data read from stream.
+     */
     private static String getAllDataFromStream(InputStream inputStream) {
         StringBuilder builder = new StringBuilder();
         try (
@@ -48,6 +69,11 @@ public abstract class FileIOManager {
         }
     }
 
+    /**
+     * Get a list of files contained in the directory at the specified path.
+     * @param directoryPath path to directory.
+     * @return the list of names of the files found in the directory (folders excluded).
+     */
     public static List<String> getFilesInDirectory(String directoryPath) {
         File directory = new File(directoryPath);
         if (!directory.exists() || !directory.isDirectory()) {
